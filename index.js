ac5dea04d5662de3631a204089ddf6f105aefb79
VimalDev-Suriya <suriyadevaraj4@gmail.com>
2022-01-15 16:28:29 +0530

    1 file modified

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bubbleSortHeading = document.createElement('h1');
 const bubbleSortInput = document.createElement('p');
 const bubbleSortResult = document.createElement('p');
 
-const myArray = [5, 3, 8, 4, 6];
+const myArray = [5, 3, 8, 4, 1];
 bubbleSortInput.innerHTML = 'INPUT = ' + myArray;
 bubbleSortHeading.innerHTML = 'Bubble Sort';
 bubbleSortWrapper.append(bubbleSortHeading);
@@ -40,11 +40,36 @@ function sanitize(str) {
     .join('');
 }
 
-function isAnagram(str1, str2) {
-  if (str1.length !== str2.length) return false;
-  return sanitize(str1) === sanitize(str2);
+// COMPARING CHARECTER MAP
+function characterMap(str) {
+  let charObj = {};
+  for (let charecter of str) {
+    if (charObj.hasOwnProperty(charecter)) {
+      charObj[charecter]++;
+    } else {
+      charObj[charecter] = 1;
+    }
+  }
+  return charObj;
 }
 
-console.log("DIRECT METHOD",isAnagram('indian', 'ndiani'));
+function isAnagram(str1, str2, methodName) {
+  if (str1.length !== str2.length) return false;
 
+  if (methodName === 'directMethod') {
+    return sanitize(str1) === sanitize(str2);
+  } else {
+    const charAtStr1 = characterMap(str1);
+    const charAtStr2 = characterMap(str2);
 
+    for (let val in charAtStr1) {
+      if (charAtStr1[val] !== charAtStr2[val]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+console.log('DIRECT METHOD', isAnagram('indian', 'ndiani', 'directMethod'));
+console.log('CHARCTER MAP', isAnagram('andian', 'dianin'));
